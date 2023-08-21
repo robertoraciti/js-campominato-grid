1738019c17f74ca606cb4b6253e534ff9a52c9ad
robertoraciti <rob.raciti97@gmail.com>
2023-08-21 15:15:19 +0200

    complete ex without bonus

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Consegna
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+const cellsContainer = document.getElementById("cells-container");
+const playButton = document.getElementById("play-button");
+const cellsTotal = 10 * 10;
+
+playButton.addEventListener("click", function () {
+  cellsContainer.innerHTML = "";
+
+  for (let i = 1; i <= cellsTotal; i++) {
+    generateGrid(cellsContainer, i);
+  }
+});
+
+function generateGrid(container, i) {
+  const cell = document.createElement("li");
+  cell.innerText = i;
+  cell.classList.add("cell");
+
+  cell.addEventListener("click", function () {
+    cell.classList.toggle("selected-cell");
+    console.log(cell.innerText);
+  });
+
+  container.append(cell);
+}
